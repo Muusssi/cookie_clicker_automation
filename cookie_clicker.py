bd53ea2b96e369eee44809b6c1df5845689aa31b
Tommi Oinonen <tommi.oinonen@gmail.com>
2025-04-01 17:24:22 +0300

    product boying optimized and handles billions

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -1,22 +1,67 @@
 
+import time
 
 from engine import CookieClicker, Product
 
 engine = CookieClicker(headless=False)
 
 
-while True:
-    engine.click_big_cookie()
-    cookies, per_second = engine.get_cookie_counts()
-    prices = engine.get_product_info()
 
-    if cookies >= prices[Product.Mine]:
-        engine.buy_product(Product.Mine)
-    elif cookies >= prices[Product.Farm]:
-        engine.buy_product(Product.Farm)
-    elif cookies >= prices[Product.Grandma]:
-        engine.buy_product(Product.Grandma)
-    elif cookies >= prices[Product.Cursor]:
-        engine.buy_product(Product.Cursor)
+def select_next_product(product_info, base_cps) -> Product:
+    for product in product_info:
+        product_info[product]['time_required'] = product_info[product]['price']/base_cps
+        advantage_point = product_info[product]['time_required']
+        if product_info[product]['cps']:
+            advantage_point = product_info[product]['price'] * (base_cps + product_info[product]['cps']) / (base_cps*product_info[product]['cps'])
+        product_info[product]['advantage_point'] = advantage_point
+    best_product = sorted(list(product_info.values()), key=lambda x: x['advantage_point'])[0]
+    print('Next product:', best_product['product'], best_product['price'], best_product['time_required'], 'sec')
+    return best_product['product']
+
+def select_next_upgrade(upgrades) -> str:
+    if upgrades:
+        print('Next upgrade:', upgrades[0]['price'], upgrades[0]['upgrade'])
+        if not upgrades[0].get('price', None):
+            return None
+        return upgrades[0]['index']
+    return None
 
 
+def main():
+    active_cps = engine.measure_active_clicking()
+
+    next_product = None
+    next_upgrade = None
+    product_info = {}
+    upgrades = []
+    while True:
+        cookies, cps = engine.get_cookie_counts()
+        if active_cps > 0.0001*cps:
+            engine.click_big_cookie()
+        else:
+            active_cps = 0
+
+        if next_upgrade is None:
+            upgrades = engine.get_updagrade_info()
+            next_upgrade = select_next_upgrade(upgrades)
+
+        if next_upgrade is not None and upgrades[next_upgrade]['price'] <= cookies:
+            engine.buy_upgrade(next_upgrade)
+            next_product = None
+            next_upgrade = None
+            active_cps = engine.measure_active_clicking()
+
+        if not next_product:
+            product_info = engine.get_product_info()
+            next_product = select_next_product(product_info, cps + active_cps)
+
+        if next_product and product_info[next_product]['price'] <= cookies:
+            engine.buy_product(next_product)
+            next_product = None
+            next_upgrade = None
+            active_cps = engine.measure_active_clicking()
+
+
+
+if __name__ == '__main__':
+    main()
--- engine.py
+++ engine.py
@@ -2,10 +2,18 @@
 import re
 import time
 from enum import Enum
+from collections import defaultdict
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 
-COOKIE_COUNT_REGEX = r"(?P<cookies>[0-9,]+) cookies? per second: (?P<per_second>[0-9,]+(.[0-9]+)?)"
+
+
+COOKIE_COUNT_REGEX = r"(?P<cookies>[0-9,]+(\.[0-9]+)?( million)?( billion)?) cookies? per second: (?P<cps>[0-9,]+(\.[0-9]+)?( million)?( billion)?)"
+
+PRODUCT_TOOLTIP_REGEX = r"^(?P<price>[0-9,]+(\.[0-9]+)?( million)?( billion)?) (?P<product>.+) owned: (?P<owned>[0-9,]+) .+\. each .+ produces (?P<cps>[0-9,]+(\.[0-9]+)?( million)?( billion)?) cookies? per second .+$"
+
+UPGRADE_TOOLTIP_REGEX = r"^(?P<price>[0-9,]+(\.[0-9]+)?( million)?( billion)?) (?P<upgrade>.+)$"
+
 
 
 class Product(Enum):
@@ -13,12 +21,20 @@ class Product(Enum):
     Grandma = 1
     Farm = 2
     Mine = 3
-
-def _parse_int(str_value) -> int:
-    return int(str_value.replace(',', ''))
+    Factory = 4
+    Bank = 5
+    Temple = 6
+    WizardTower = 7
 
 def _parse_float(str_value) -> float:
-    return float(str_value.replace(',', ''))
+    multiplier = 1
+    if ' million' in str_value:
+        str_value = str_value.replace(' million', '')
+        multiplier = 1_000_000
+    if ' billion' in str_value:
+        str_value = str_value.replace(' billion', '')
+        multiplier = 1_000_000_000
+    return float(str_value.replace(',', ''))*multiplier
 
 class CookieClicker():
     def __init__(self, *, headless: bool = False):
@@ -45,48 +61,114 @@ class CookieClicker():
         self.page.locator("#bigCookie").click()
 
     def get_cookie_counts(self) -> tuple[int, int]:
-        cookies, per_second = 0, 0
+        cookies, cps = 0, 0
         cookie_counts_text = self.page.locator("#cookies").inner_text().replace('\n', ' ')
         result = re.match(COOKIE_COUNT_REGEX, cookie_counts_text)
         if result:
-            cookies = _parse_int(result.group('cookies'))
-            per_second = _parse_float(result.group('per_second'))
-        print(f"Cookies: {cookies}, CpS: {per_second}")
-        return cookies, per_second
+            cookies = _parse_float(result.group('cookies'))
+            cps = _parse_float(result.group('cps'))
+        #print(f"Cookies: {cookies}, CpS: {cps}")
+        return cookies, cps
 
     def get_product_info(self) -> dict[Product, int]:
-        prices = {}
+        info = defaultdict(lambda: {})
+        previous_cps = 0
         for product in Product:
+            info[product]['product'] = product
             price_string = self.page.locator(f"#productPrice{product.value}").inner_text()
-            prices[product] = int(price_string.replace(',',''))
-        return prices
+            info[product]['price'] = _parse_float(price_string)
+            if info[product]['price']:
+                tooltip_data = self.get_product_tooltip_data(product)
+                info[product]['owned'] = tooltip_data.get('owned', 0)
+                info[product]['cps'] = tooltip_data.get('cps', 0)
+                info[product]['cpspc'] = (info[product]['cps'] or previous_cps)/info[product]['price']
+                previous_cps = info[product]['cps']
+        return info
+
+    def get_product_tooltip_data(self, product) -> dict:
+        try:
+            self.page.locator(f"div#product{product.value}").hover(timeout=100)
+            return self._parse_product_tooltip(self.read_tooltip_text())
+        except PlaywrightTimeoutError:
+            pass
+        return {}
 
     def buy_product(self, product: Product):
         self.page.locator(f"#product{product.value}").click()
-        print(f"Bought {product}")
+        #print(f"Bought {product}")
 
-    def check_product_info(self):
-        self.page.locator("div#product0").hover()
-        self.read_tool_tip()
+    def _parse_product_tooltip(self, tooltip_string) -> dict:
+        if not tooltip_string:
+            return {}
+        tooltip_string = tooltip_string.replace('\n', ' ')
+        # print('----------------')
+        # print(tooltip_string)
+        result = re.match(PRODUCT_TOOLTIP_REGEX, tooltip_string)
+        # print(result.groups() if result else None)
+        # print('----------------')
+        if not result:
+            return {}
+        return {
+            'price': _parse_float(result.group('price')),
+            'owned': _parse_float(result.group('owned')),
+            'cps': _parse_float(result.group('cps')),
+        }
 
-    def check_upgrades(self):
-        #self.page.locator("div.upgrade")
-        self.page.locator("div#upgrade0").hover()
-        print(self.page.locator("#tooltipCrate").inner_html())
-
-    def read_tool_tip(self):
+    def read_tooltip_text(self) -> str:
         if self.page.query_selector('#tooltip'):
-            print(self.page.locator("#tooltip").inner_html())
-        else:
-            print("Tooltip not found")
+            string = self.page.locator("#tooltip").inner_text()
+            return string
+        print("Tooltip not found")
+        return None
+
+    def get_updagrade_info(self) -> list[dict]:
+        upgrades = []
+        for index in range(3):
+            if not self.page.query_selector(f"div#upgrade{index}"):
+                continue
+            self.page.locator(f"div#upgrade{index}").hover()
+            text = self._read_tooltip_crate()
+            upgrade = self._parse_upgrade_tooltip(text)
+            upgrade['index'] = index
+            upgrades.append(upgrade)
+        return upgrades
+
+    def _read_tooltip_crate(self):
+        if self.page.query_selector('#tooltipCrate'):
+            return self.page.locator("#tooltipCrate").inner_text()
+        print("TooltipCrate not found")
+        return None
+
+    def _parse_upgrade_tooltip(self, tooltip_string) -> dict:
+        if not tooltip_string:
+            return {}
+        tooltip_string = tooltip_string.replace('\n', ' ')
+        result = re.match(UPGRADE_TOOLTIP_REGEX, tooltip_string)
+        if not result:
+            print('Unable to parse:')
+            print(tooltip_string)
+            return {}
+        return {
+            'price': _parse_float(result.group('price')),
+            'upgrade': result.group('upgrade'),
+        }
+
+    def buy_upgrade(self, index):
+        print("Buying upgrade")
+        self.page.locator(f"div#upgrade{index}").hover()
+        text = self.page.locator("#tooltipCrate").inner_text()
+        print(text.replace('\n', ' '))
+        self.page.locator(f"div#upgrade{index}").click()
 
 
-
-
-
-# div #tooltipCrate
-
-# div#upgrades
-# div#upgrade0
-
-
+    def measure_active_clicking(self, *, minimum_cookies: int = 100, minimum_time: float = 3) -> float:
+        original_cookies, original_cps = self.get_cookie_counts()
+        cookies = original_cookies
+        start = time.time()
+        while cookies - original_cookies < minimum_cookies or time.time() - start < minimum_time:
+            self.click_big_cookie()
+            cookies, _ = self.get_cookie_counts()
+        total_cps = (cookies - original_cookies)/(time.time() - start)
+        active_cps = total_cps - original_cps
+        print(f"Active click speed: {active_cps} cps")
+        return active_cps
